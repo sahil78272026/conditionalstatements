2b337918109f73d67e554daae1234447bd1c16c7
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-04 23:14:12 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,26 @@
+a = 45
+
+# if-elif-else ladder
+if (a<3):
+    print("a is greater than 3")
+
+elif(a<30):
+    print("a is greater than 30")
+
+else:
+    print("a is not greater than 3 or 30")
+
+# indvidual if 
+
+if (a>50):
+    print(" a is greater than 50")
+
+if (a<70):
+    print("a is less than 70")
+
+if (a<100):
+    print("a is less than 100")
+else: print("a is greater than 100")
+
+print("Done")
+
